6a7cf694f653a08df263dcac1a2ba5c5d1ffe0fd
Enache Ionut <yonutz_1695@yahoo.com>
2017-03-22 12:26:17 +0200

    Add MultiThread operation using a matrix of threads

---
+++ MultiThread Matrix Multiplication - Thread Var/Multiplication of two matrix/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Multiplication_of_two_matrix
+{
+    public class Element
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+    }
+}
